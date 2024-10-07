e6ebe01bb6dad4d14082e213441e3623d8b02ddc
Guilherme Ferreira <106194282+guixande0905g@users.noreply.github.com>
2024-10-07 11:39:04 -0300

    Add files via upload

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,8 @@
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json";
+
+async function quantidadeUsuarios(){
+    const res = await fetch(url);
+    const dados = await res.json();
+    console.log(dados);
+}
+quantidadeUsuarios();
